4c80d03ce72fbdbfe9fc802391bfd5b86c613189
dont starve together <dst@bitsmasher.net>
2026-06-02 21:17:34 -0600

    get it working on chonk

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,30 +1,6 @@
 --- @file modoverrides.lua
 -- @brief This file determines which mods to load. It must be identical for each shard. 
 return {
-    ["workshop-3138571948"]={
-        configuration_options={ },
-        enabled=true
-    },
-    ["workshop-373991022"]={
-        configuration_options={ },
-        enabled=true
-    },
-    ["workshop-3361016346"]={
-        configuration_options={ },
-        enabled=true
-    },
-    ["workshop-398858801"]={
-        configuration_options={ },
-        enabled=true
-    },
-    ["workshop-1751811434"]={
-        configuration_options={ },
-        enabled=true
-    },
-    ["workshop-2880198735"]={
-        configuration_options={ },
-        enabled=true
-    },
     ["workshop-1077747217"] = {
         enabled = true,
         configuration_options = {
